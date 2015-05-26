a294035d39655725f5af3500a85b34a802c1fd3a
Jesus Marin <jesusangelm@gmail.com>
2015-05-26 09:29:58 -0430

    basic index action for Page Controller.

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -1,4 +1,12 @@
 class PageController < ApplicationController
+  respond_to :html, :js
   def index
+    if params[:q]
+      @classifieds = Classified.where("content @@ :query", query: params[:q])
+    else
+      @classifieds = Classified.order(created_at: :ASC)
+    end
+    @classifieds = Classified.order(created_at: :ASC) if params[:q].nil? || params[:q].empty?
+
   end
 end
