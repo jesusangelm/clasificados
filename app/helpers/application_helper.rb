1e55cb2a0b8a13615a1e2b13f6546a929d967ac4
Jesus Marin <jesusangelm@gmail.com>
2015-05-26 09:32:35 -0430

    add custom helpers for time and classified type formatting

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+  def classified_type_formater(type)
+    type == "apartment_rent" ? "Apartamento" : "Casa"
+  end
+
+  def date_formater(date)
+    date.strftime("%e/%m/%Y %l:%M %p")
+  end
 end
