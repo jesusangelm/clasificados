75e61eedabb88924904492398d7d288ffe787aa0
Jesus Marin <jesusangelm@gmail.com>
2015-05-27 21:16:53 -0430

    show classsifieds order by created date DESC

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -2,11 +2,11 @@ class PageController < ApplicationController
   respond_to :html, :js
   def index
     if params[:q]
-      @classifieds = Classified.where("content @@ :query", query: params[:q])
+      @classifieds = Classified.where("content @@ :query", query: params[:q]).order(created_at: :DESC)
     else
-      @classifieds = Classified.order(created_at: :ASC)
+      @classifieds = Classified.order(created_at: :DESC)
     end
-    @classifieds = Classified.order(created_at: :ASC) if params[:q].nil? || params[:q].empty?
+    @classifieds = Classified.order(created_at: :DESC) if params[:q].nil? || params[:q].empty?
 
   end
 end
