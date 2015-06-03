9d76ef7d05bf32cd6e94a82fdc19bbf9c23fd7ab
Jesus Marin <jesusangelm@gmail.com>
2015-06-03 14:17:21 -0430

    callback for check the current saved classified match with any keywords of users

--- app/models/classified.rb
+++ app/models/classified.rb
@@ -1,3 +1,17 @@
 class Classified < ActiveRecord::Base
   validates :content, uniqueness: true
+
+  after_create :find_matched_keywords
+
+  def find_matched_keywords
+    User.all.each do |u|
+      u.profile.keywords.scan(/[a-zA-Z\d]+/) do |k|
+        if self[:content].include?(k)
+          NotificationMailer.keyword_found(self, u).deliver_later
+          break
+        end
+      end
+    end
+  end
+
 end
