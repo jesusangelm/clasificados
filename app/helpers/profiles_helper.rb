f7a601b66a7a8674123365952144701b5368c15b
Jesus Marin <jesusangelm@gmail.com>
2015-06-03 09:43:40 -0430

    add some helpers methods for string proccesing and keywords search

--- app/helpers/profiles_helper.rb
+++ app/helpers/profiles_helper.rb
@@ -1,2 +1,10 @@
 module ProfilesHelper
+
+  def words_selector_regex(user_keywords)
+    user_keywords.scan(/[a-zA-Z\d]+/)
+  end
+
+  def classifieds_by_keyword(keyword)
+    Classified.where("content @@ :q", q: keyword)
+  end
 end
